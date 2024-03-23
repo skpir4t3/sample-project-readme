8f32aec1ae294ff6e17f6626502988b9ce680cda
skpir4t3 <skg11786@gmail.com>
2024-03-23 14:46:05 +0530

    fdhsduf

---
+++ src/main/java/org/example/AddSub.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class AddSub {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int a = 4, b = 7;
+        System.out.println(add(a, b));
+    }
+}
